3911651b1ce932aa1ce07958e3c2f9394ab6a409
deven <panchaldevendra522@gmail.com>
2021-06-28 22:45:12 +0530

    remove link

--- src/components/NavbarButton.js
+++ src/components/NavbarButton.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link, NavLink } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 function NavbarButton() {
   return (
     <div className="m-2 flex flex-row  p-2 mb-5 space-x-5">
