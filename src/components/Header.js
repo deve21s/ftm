0ea46fbd3c46cd87928ea0dfe770259be0531ef7
deven <panchaldevendra522@gmail.com>
2021-07-14 16:37:29 +0530

    added navlink to main link & logout

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,38 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, NavLink, Redirect } from "react-router-dom";
 
 function Header() {
+  const token = localStorage.getItem("token");
+  const logout = () => {
+    localStorage.removeItem("token");
+    return <Redirect to="/" />;
+  };
+
   return (
     <div className="w-full flex justify-center space-x-2 font-thin text-xl bg-white p-2 fixed z-10">
-      <Link
+      <NavLink
         className="ring-2 px-2 ring-green-400 hover:bg-green-400"
         to="/family"
+        activeClassName="bg-green-400"
       >
         Family
-      </Link>
-      <Link
+      </NavLink>
+      <NavLink
         className="ring-2 px-2 ring-green-400 hover:bg-green-400"
         to="/tasks"
+        activeClassName="bg-green-400"
       >
         Tasks
-      </Link>
+      </NavLink>
+      {token && (
+        <Link
+          onClick={logout}
+          className="ring-2 px-2 ring-green-400 hover:bg-green-400"
+          to="/logout"
+        >
+          Logout
+        </Link>
+      )}
     </div>
   );
 }
